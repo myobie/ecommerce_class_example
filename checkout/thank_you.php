5a834a76b87128025c9bde46892651a1ae8cdbae
Nathan Herald <myobie@mac.com>
2010-03-10 14:38:37 -0500

    Spruced up the thank you page.

--- checkout/thank_you.php
+++ checkout/thank_you.php
@@ -1,1 +1,16 @@
-Thank you :)
+<?
+
+$dir = dirname(__FILE__);
+require_once "$dir/../app/requires.php";
+$db = new DB();
+
+$cart = find_or_create_cart();
+
+include "../app/includes/header.php";
+
+?>
+
+<h2>Thank you for your order.</h2>
+<p>You will be notified by email when your item is shipped.</p>
+    
+<? include "../app/includes/footer.php" ?>
