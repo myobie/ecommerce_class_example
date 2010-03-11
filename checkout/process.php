e15d9928c514d2d3b898f784732a8f40a334e2d0
Nathan Herald <myobie@mac.com>
2010-03-11 08:32:14 -0500

    Put in some code but didn't test it.

--- checkout/process.php
+++ checkout/process.php
@@ -26,19 +26,24 @@ if ($cart_result->num_rows < 1)
   $_SESSION["cart_id"] = $cart_id;
 }
 
+$result = $m->query("SELECT ci.id, ci.quantity, ci.variant_id, 
+                            v.price, p.default_price, v.color_id, v.size_id
+                     FROM   cart_items ci, variants v, products p
+                     WHERE  ci.variant_id = v.id
+                     AND    v.product_id = p.id
+                     ");
+
 $cart_items = mysqli_result_to_array($result);
 
-array_walk($cart_items, function (&$cart_item, $key) {
-  global $m;
+foreach ($cart_items as $cart_item)
+{
   if (gettype($cart_item["price"]) == "NULL")
   {
-    $product_id = $cart_item["product_id"];
-    $result = $m->query("SELECT id, default_price FROM products WHERE id = '$product_id'");
-    $temp_products = mysqli_result_to_array($result);
-    $temp_product = $temp_products[0];
-    $cart_item["price"] = $temp_product["default_price"];
+    $cart_item["price"] = $cart_item["default_price"]
   }
-});
+}
+
+print_r($cart_items);
 
 $total_func = create_function('$sum, $row', 'return $sum += ($row["quantity"] * $row["price"]);');
 $cart_total = array_reduce($cart_items, $total_func, 0);
@@ -66,25 +71,54 @@ $result = Braintree_Transaction::sale(array(
     )
 ));
 
-echo 'Transaction ID: ' . $result->transaction->id;
-echo 'Status: ' . $result->transaction->status;
-
-
-
 if ($result->success)
 {
   
-  /*
-    - create an order record
-    - create an order item for each cart item
-    - delete all the cart items
-    - delete the cart
-  */ 
+  $status = $result->transaction->status;
+  $transaction_key = $result->transaction->id;
+  
+  $m->query("INSERT INTO orders SET total = '$cart_total', 
+                                    status = '$status',
+                                    transaction_key = '$transaction_key'");
+  
+  $order_id = $m->insert_id;
+  
+  foreach ($cart_items as $cart_item)
+  {
+    $quantity = $cart_item["quantity"];
+    $price = $cart_item["price"];
+    $name = $cart_item["name"];
+    $color_id = $cart_item["color_id"];
+    $size_id = $cart_item["size_id"];
+    $subtotal = $cart_item["price"] * $cart_item["quantity"];
+    $variant_id = $cart_item["variant_id"];
+    
+    $m->query("INSERT INTO order_items
+               SET order_id = '$order_id'
+                   quantity = '$quantity'
+                   price_per_variant = '$price'
+                   name = '$name'
+                   color_id = '$color_id'
+                   size_id = '$size_id'
+                   subtotal = '$subtotal'
+                   variant_id = '$variant_id'
+               ");
+  }
+  
+  $m->query("DELETE FROM cart_items WHERE cart_id = '$cart_id'");
+  $m->query("DELETE FROM carts WHERE id = '$cart_id'");
   
   // header('Location: /checkout/thank_you.php');
 } else {
   
   // --- There was an error! ---
+  
+  $status = $result->transaction->status;
+  $transaction_key = $result->transaction->id;
+  
+  $m->query("INSERT INTO orders SET total = '$cart_total', 
+                                    status = '$status',
+                                    error_message = 'error'");
   
   include "../app/includes/header.php";
   
